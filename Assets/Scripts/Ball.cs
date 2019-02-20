1644b03f71f18c5119b51ee2a2f2104a19d79c07
Julian Hernandez <gmfk07@gmail.com>
2019-02-20 15:02:44 -0500

    Rolly Poley Oley

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public int lane = 0;
     public int maxLane = 3;
     public float laneDist = 0.5f;
+    public float accel = 2f;
+    public float maxVelocity = 7f;
+
+    private int lane = 0;
+    private float velocity = 0;
 
     // Update is called once per frame
     void Update()
@@ -15,6 +19,12 @@ public class Ball : MonoBehaviour
             switchLanes(1);
         if (Input.GetKeyDown(KeyCode.S))
             switchLanes(-1);
+    }
+
+    void FixedUpdate()
+    {
+        gameObject.transform.position += new Vector3(velocity*Time.deltaTime, 0, 0);
+        velocity = Mathf.Max(maxVelocity, velocity + accel * Time.deltaTime);
     }
 
     private void switchLanes(int laneAmt)
