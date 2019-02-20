2fc04158921dae0554f1128e9df5aeafb2d1ea59
Julian Hernandez <gmfk07@gmail.com>
2019-02-20 14:50:22 -0500

    Camera Follow

---
+++ Assets/Scripts/FollowBall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBall : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(target.position.x, 0, -10);
+    }
+}
