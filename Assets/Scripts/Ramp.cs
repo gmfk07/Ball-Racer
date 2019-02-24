58f7bc338351fd1a167c345269d47ea0a7ce3a16
Julian Hernandez <gmfk07@gmail.com>
2019-02-24 18:17:35 -0500

    Early Ramp

---
+++ Assets/Scripts/Ramp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ramp : MonoBehaviour
+{
+    public float height = 2;
+    //What's the normal acceleration of gravity?
+    public float gravityAccel = 9.8f;
+
+    private float theta;
+    private float hypotenuse;
+
+    //What's the top-down deceleration on this slope?
+    public float slopeAccel;
+    private float groundToSlopeMult;
+
+    void Start()
+    {
+        float length = transform.localScale.x;
+        theta = Mathf.Atan(height/length);
+        hypotenuse = Mathf.Sqrt(Mathf.Pow(height, 2) + Mathf.Pow(length, 2));
+        slopeAccel = gravityAccel * Mathf.Sin(theta);
+        groundToSlopeMult = length / hypotenuse;
+    }
+}
