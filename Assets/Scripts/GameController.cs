5a25e88538bbdadc0614030665f73658f3494714
Temi Taylor <temit@mit.edu>
2019-02-23 19:01:07 -0500

    Implementing countdown in game controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /**
  * This handles the overall flow of the game (starting/finishing)
@@ -10,10 +11,12 @@ using UnityEngine;
  */
 public class GameController : MonoBehaviour
 {
+    public GameObject canvasPrefab;
+
     //debug variables to check if things are working from the editor; should remove later
+    [Header("Debug")]
     public bool debugGameStarted;
     public bool debugGameFinished;
-
     
     void Start()
     {
@@ -60,6 +63,45 @@ public class GameController : MonoBehaviour
     /// </summary>
     /// <returns>Returns true iff both balls have reached the finish line</returns>
     public bool gameFinished()
+    {
+        throw new System.Exception("not implemented");
+    }
+
+
+
+    //// Private Methods
+    
+    /// <summary>
+    /// Performs the countdown for the start of a race.
+    /// 
+    /// Returning an IEnumerator allows this to run in parallel with other things, so that
+    /// the second-long waiting between counts doesn't mess anything else up.
+    /// </summary>
+    /// <returns>whatever IEnumerators do to yield things</returns>
+    private IEnumerator doCountdown()
+    {
+        var countdownCanvas = Instantiate(this.canvasPrefab, this.transform) as GameObject;
+        Text countdownText = countdownCanvas.GetComponentInChildren<Text>();
+
+        //do the countdown, yielding for a second between counts;
+        for (int i = 3; i > 0; i --)
+        {
+            countdownText.text = i.ToString();
+            yield return new WaitForSeconds(1);
+        }
+
+        //Start the race, then wait for a second before removing canvas
+        countdownText.text = "Go!";
+        startRace();
+        yield return new WaitForSeconds(1);
+
+        Destroy(countdownCanvas);
+    }
+
+    /// <summary>
+    /// Cues the balls to begin moving.
+    /// </summary>
+    private void startRace()
     {
         throw new System.Exception("not implemented");
     }
