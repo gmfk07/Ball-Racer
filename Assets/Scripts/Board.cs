9c68544a1e4f74f6d121e115b16d98e0f60e13bc
Temi Taylor <temit@mit.edu>
2019-02-23 20:00:24 -0500

    Making a small board script, will be useful for finish condition

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This holds some useful data for the board, mostly related to its size
+ * 
+ * May be useful for placing terrain later
+ */
+public class Board : MonoBehaviour
+{
+    public int boardSize = 100;
+
+    void Start()
+    {
+        //scale board to defined size
+        Vector3 originalScale = transform.localScale;
+        transform.localScale = new Vector3(boardSize, originalScale.y, originalScale.z);
+    }
+}
